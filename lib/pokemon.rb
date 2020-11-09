1b295589c553331fffa682ebb60f2e57ebd381e2
tchartchke <chartai.kl@gmail.com>
2020-11-09 00:31:43 -0600

    updated pokemon class

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,16 +1,22 @@
+# frozen_string_literal: true
+
 class Pokemon
   attr_accessor :name, :types, :id
 
+  @@all = Set.new
+
   def initialize(attributes)
-    attributes.each do |key, value|
-      self.send("#{key.downcase}=", value) if self.class.method_defined?(key.downcase)
-      if key == 'types'
-        @types = []
-        value.each do |t|
-          @types << t['type']['name']
-        end
-      end
+    @name = attributes['name']
+    @id = attributes['id']
+    @types = []
+    attributes['types'].each do |t|
+      @types << t['type']['name']
     end
+    @@all.add(self) if !@@all.find { |entry| entry.name == name}
+    self
   end
-  
+
+  def self.all
+    @@all
+  end
 end
