ff44fa13e6bb4beadccbe65bb7e44f7269f7bc89
tchartchke <chartai.kl@gmail.com>
2020-11-09 10:37:02 -0600

    updated pokedex

--- lib/pokedex.rb
+++ lib/pokedex.rb
@@ -1,34 +1,59 @@
 # frozen_string_literal: true
 
 class Pokedex
-  attr_accessor :scanned_in, :pokemon, :owner
+  attr_accessor :pokemon, :owner
 
   @@all = Set.new
 
   def initialize(owner)
     @owner = owner
-    @scanned_in = 0
     @pokemon = Set.new
     @@all.add(self)
   end
 
-  def scan_pokemon(name)
-    if !found?(name)
-      new_pokemon = Api.new.get_pokemon_by_name(name)
-      pokemon.add?(new_pokemon)
-      self.scanned_in += 1
-    else
-      puts "#{name} is already in your pokedex"
-    end
+  def scan(identifier)
+    poke = Pokedex.discovered?(identifier)
+    poke ||= discover_new_pokemon(identifier)
+    pokemon.add?(poke) unless seen?(poke.name)
   end
 
-  def get_id(name)
-    poke = found?(name)
-    poke ? poke.id : nil
+  def discover_new_pokemon(identifier)
+    Api.new.get_pokemon(identifier)
   end
 
-  def found?(name)
-    pokemon.find { |entry| entry.name == name}
+  def get_pokemon_id_or_name(identifier)
+    poke = seen?(identifier)
+    poke ? poke.send(Pokedex.flip(identifier)) : nil
+  end
+
+  def get_type(identifier)
+    poke = seen?(identifier)
+    poke ? poke.types : nil
+  end
+
+  def seen?(identifier)
+    return nil if pokemon.empty?
+
+    pokemon.find { |p| p.send(Pokedex.name_or_id(identifier)) == identifier }
+  end
+
+  def seen
+    pokemon.size
+  end
+
+  def self.discovered?(identifier)
+    return nil if Pokemon.all.empty?
+
+    Pokemon.all.find { |p| p.send(name_or_id(identifier)) == identifier }
+  end
+
+  def self.name_or_id(identifier)
+    # binding.pry
+    identifier.to_i.positive? ? 'id' : 'name'
+  end
+
+  def self.flip(identifier)
+    identifier.to_i.positive? ? 'name' : 'id'
   end
 
   def self.all
